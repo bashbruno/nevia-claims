c04225d207ec958e12571f6f6e63816ed2fa835b
Bruno Santos <bruno.santos@sitewerk.ch>
2026-01-01 22:17:18 +0000

    chore: add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,11 @@ export const links: Route.LinksFunction = () => [
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css2?family=Onest:wght@100..900&display=swap',
   },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    href: '/favicon.png',
+  },
 ]
 
 export function Layout({ children }: { children: React.ReactNode }) {
